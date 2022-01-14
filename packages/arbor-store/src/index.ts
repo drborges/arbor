a7313ebb0f8e2a27928b39600a2d5a9faf2e02ac
Diego Borges <drborges.cic@gmail.com>
2022-01-14 18:38:58 -0300

    Export Model helper class

--- packages/arbor-store/src/index.ts
+++ packages/arbor-store/src/index.ts
@@ -1,8 +1,9 @@
 import Path from "./Path"
 import Arbor from "./Arbor"
+import Model from "./Model"
 import stitch from "./stitch"
 import Collection from "./Collection"
 import { clone, clonable } from "./cloning"
 
 export * from "./types"
-export { clone, clonable, Collection, stitch, Path, Arbor as default }
+export { clone, clonable, Collection, Model, stitch, Path, Arbor as default }
