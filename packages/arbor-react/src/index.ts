6a0478fca48a051b78ebcb10ede16baf92303885
Diego Borges <drborges.cic@gmail.com>
2022-05-12 20:45:30 -0300

    Remove useArborValue hook

--- packages/arbor-react/src/index.ts
+++ packages/arbor-react/src/index.ts
@@ -8,13 +8,11 @@ import Arbor, {
 
 import useArbor from "./useArbor"
 import useArborNode from "./useArborNode"
-import useArborValue from "./useArborValue"
 
 export {
   Collection,
   useArbor,
   useArborNode,
-  useArborValue,
   Path,
   produce,
   MutationMode,
