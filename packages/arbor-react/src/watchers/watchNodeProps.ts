5c1d810586a19f77d8e38e3acd560cac3bf220fd
Diego Borges <drborges.cic@gmail.com>
2022-05-13 16:45:03 -0300

    Implement watchNodeProps watcher

---
+++ packages/arbor-react/src/watchers/watchNodeProps.ts
@@ -0,0 +1,16 @@
+import { ArborNode, isNode, MutationEvent } from "@arborjs/store";
+
+type PropOf<T extends object> = keyof T
+
+export function watchNodeProps<T extends object>(...props: PropOf<T>[]) {
+  return (node: ArborNode<T>) =>
+    (event: MutationEvent<T>) => {
+      if (!isNode(node)) return false
+      if (!event.mutationPath.is(node.$path)) return false
+
+      const previousNodeValue = event.mutationPath.walk(event.state.previous) as T
+      const currentNodeValue = event.mutationPath.walk(event.state.current) as T
+
+      return props.some(prop => previousNodeValue[prop] !== currentNodeValue[prop])
+    }
+}
