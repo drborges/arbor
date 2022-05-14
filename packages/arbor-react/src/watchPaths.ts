42656f489904d1d6c3692f5b02da0111eb05783b
Diego Borges <drborges.cic@gmail.com>
2022-05-14 09:14:41 -0300

    Optimize watchPaths watcher

--- packages/arbor-react/src/watchPaths.ts
+++ packages/arbor-react/src/watchPaths.ts
@@ -1,30 +1,37 @@
-import { ArborNode, isNode, MutationEvent } from "@arborjs/store";
+import { ArborNode, isNode, MutationEvent } from "@arborjs/store"
 
-const parsePathPattern = (pattern: string) => {
-  const processedWildcards = pattern.replace(/:\w+/g, ".*")
+const parsePathPattern = (pathPattern: string): [RegExp, string | null] => {
+  const processedWildcards = pathPattern.replace(/:\w+/g, ".*")
   const patternParts = processedWildcards.split("/")
   const lastPartIndex = patternParts.length - 1
   const lastPatternPart = patternParts[lastPartIndex]
   const endsWithPropName = lastPatternPart.startsWith("#")
-  const regex = endsWithPropName ? patternParts.slice(0, lastPartIndex).join("/") : processedWildcards
-  return [`^${regex}$`, endsWithPropName ? lastPatternPart.slice(1) : null]
+  const pattern = endsWithPropName
+    ? patternParts.slice(0, lastPartIndex).join("/")
+    : processedWildcards
+  const regex = new RegExp(`^${pattern}$`)
+
+  return [regex, endsWithPropName ? lastPatternPart.slice(1) : null]
 }
 
-export function watchPaths(...pathPatterns: string[]) {
+export function watchPaths(...patterns: string[]) {
+  const parsedPatterns = patterns.map(parsePathPattern)
+
   return <T extends object>(node: ArborNode<T>, event: MutationEvent<T>) => {
-      if (!isNode(node)) return false
-      if (!event.mutationPath.affects(node)) return false
+    if (!isNode(node)) return false
+    if (!event.mutationPath.affects(node)) return false
 
-      return pathPatterns.some(pattern => {
-        const [path, prop] = parsePathPattern(pattern)
-        const matches = new RegExp(path).test(event.mutationPath.toString())
+    return parsedPatterns.some(([regex, prop]) => {
+      const matches = regex.test(event.mutationPath.toString())
 
-        if (!matches) return false
-        if (!prop) return true
+      if (!matches) return false
+      if (!prop) return true
 
-        const previousNodeValue = event.mutationPath.walk(event.state.previous) as T
-        const currentNodeValue = event.mutationPath.walk(event.state.current) as T
-        return previousNodeValue[prop] !== currentNodeValue[prop]
-      })
-    }
+      const previousNodeValue = event.mutationPath.walk(
+        event.state.previous
+      ) as T
+      const currentNodeValue = event.mutationPath.walk(event.state.current) as T
+      return previousNodeValue[prop] !== currentNodeValue[prop]
+    })
+  }
 }
