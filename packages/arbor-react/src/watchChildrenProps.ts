b5bedbea0c33a57b3ef660fd754157d02499f55d
Diego Borges <drborges.cic@gmail.com>
2022-05-14 09:40:04 -0300

    Fix TS warning

--- packages/arbor-react/src/watchChildrenProps.ts
+++ packages/arbor-react/src/watchChildrenProps.ts
@@ -5,11 +5,16 @@ type PropOf<T extends object> = keyof T
 
 export function watchChildrenProps<T extends object>(...props: PropOf<T>[]) {
   return <K extends object>(node: ArborNode<K>, event: MutationEvent<K>) => {
-      if (!isNode(node)) return false
-      if (event.mutationPath.is(node.$path)) return true
+    if (!isNode(node)) return false
+    if (event.mutationPath.is(node.$path)) return true
 
-      const paths = props.map(prop => node.$path.child(":any").child(`#${prop}`).toString())
+    const paths = props.map((prop) =>
+      node.$path
+        .child(":any")
+        .child(`#${String(prop)}`)
+        .toString()
+    )
 
-      return watchPaths(...paths)(node as ArborNode<K>, event)
-    }
+    return watchPaths(...paths)(node as ArborNode<K>, event)
+  }
 }
