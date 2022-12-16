50a2faeb5da929a9545dc200ba95c26bea4d23dd
Diego Borges <drborges.cic@gmail.com>
2022-12-16 20:26:20 -0300

    Remove collection items watcher

--- packages/arbor-react/src/index.ts
+++ packages/arbor-react/src/index.ts
@@ -5,6 +5,5 @@ export { watchPaths } from "./watchPaths"
 export { watchNodeProps } from "./watchNodeProps"
 export { watchAnyMutations } from "./watchAnyMutations"
 export { watchChildrenProps } from "./watchChildrenProps"
-export { watchCollectionItemProps } from "./watchCollectionItemProps"
 
 export type { Watcher } from "./useArbor"
