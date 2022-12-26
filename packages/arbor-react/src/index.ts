f97c6ff2c47ac4674544606d4793fa77c2ea99e0
Diego Borges <drborges.cic@gmail.com>
2022-12-26 20:27:10 -0300

    Rename watchAnyMutations to watchAny

--- packages/arbor-react/src/index.ts
+++ packages/arbor-react/src/index.ts
@@ -3,7 +3,7 @@ export { default } from "./useArbor"
 export { watchNode } from "./watchNode"
 export { watchPaths } from "./watchPaths"
 export { watchNodeProps } from "./watchNodeProps"
-export { watchAnyMutations } from "./watchAnyMutations"
+export { watchAny } from "./watchAny"
 export { watchChildrenProps } from "./watchChildrenProps"
 
 export type { Watcher } from "./useArbor"
