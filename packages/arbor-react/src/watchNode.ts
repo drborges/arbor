937f3eafc026a200d31e761001d2b318e57dfeea
Diego Borges <drborges.cic@gmail.com>
2022-12-26 20:58:22 -0300

    Remove redundant watcher

--- packages/arbor-react/src/watchNode.ts
+++
@@ -1,6 +0,0 @@
-import { ArborNode, MutationEvent } from "@arborjs/store"
-
-export function watchNode() {
-  return <T extends object>(node: ArborNode<T>, event: MutationEvent) =>
-    event.mutationPath.targets(node)
-}
