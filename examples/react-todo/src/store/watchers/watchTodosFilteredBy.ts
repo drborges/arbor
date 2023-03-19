16c04ba167a22058c045439ebebd7cc4f09cc987
Diego Borges <drborges.cic@gmail.com>
2023-03-19 12:01:19 -0300

    Fix todo example filtering logic

--- examples/react-todo/src/store/watchers/watchTodosFilteredBy.ts
+++ examples/react-todo/src/store/watchers/watchTodosFilteredBy.ts
@@ -1,10 +1,7 @@
-import { watchChildren, Watcher } from "@arborjs/react";
-import { Repository } from "@arborjs/store";
-import { Todo } from "../useTodos";
+import { watchChildren, Watcher } from "@arborjs/react"
+import { Repository } from "@arborjs/store"
+import { Todo } from "../useTodos"
 
-export const watchTodosFilteredBy = (filter: string): Watcher<Repository<Todo>> => (target, event) => {
-  const isTodoFilterAll = filter === "all";
-
-  return event.mutationPath.targets(target) ||
-    (!isTodoFilterAll && watchChildren<Todo>("status")(target, event));
-};
+export const watchTodosFilteredBy = (): Watcher<Repository<Todo>> => (target, event) => {
+  return event.mutationPath.targets(target) || watchChildren<Repository<Todo>>("status")(target, event)
+}
