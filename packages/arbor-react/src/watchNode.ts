eba00fdd1bac06592f73abc2d7828c79db181488
Diego Borges <drborges.cic@gmail.com>
2023-06-09 20:05:14 -0300

    Rename NodeProps type to PropsOf

--- packages/arbor-react/src/watchNode.ts
+++ packages/arbor-react/src/watchNode.ts
@@ -1,13 +1,16 @@
 import { ArborNode, MutationEvent } from "@arborjs/store"
-import { NodeProps } from "./watchChildren"
 
-export function watchNode<T extends object>(...props: NodeProps<T>[]) {
+export type PropsOf<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? never : K
+}[keyof T]
+
+export function watchNode<T extends object>(...props: PropsOf<T>[]) {
   return (node: ArborNode<T>, event: MutationEvent<T>) => {
     if (!event.mutationPath.targets(node)) return false
     if (props.length === 0) return true
 
     return event.metadata.props.some((prop) =>
-      props.includes(prop as NodeProps<T>)
+      props.includes(prop as PropsOf<T>)
     )
   }
 }
