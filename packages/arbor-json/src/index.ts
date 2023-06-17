f1ddb1c47c95b131a35211a6bf67cfd37e875d39
Diego Borges <drborges.cic@gmail.com>
2023-06-17 20:41:22 -0300

    Add some initial docs

--- packages/arbor-json/src/index.ts
+++ packages/arbor-json/src/index.ts
@@ -25,24 +25,35 @@ function isTyped(value: unknown): value is Typed {
   return value?.["$reviver"] != null && value?.["$value"] != null
 }
 
+/**
+ * Provides a more flexible alternative to JSON.stringify / JSON.parse behaviors
+ * adding support to user-defined types while adding very little boilerplate.
+ */
 export class Json {
   #revivers = new Map<string, Reviver>()
 
   /**
+   * Decorator used to mark classes as serializable by @arborjs/json.
    *
-   * NOTE: This must be an arrow function so this can be scoped to the class instance.
-   *
-   * @param target
-   * @param _context
-   * @returns
+   * Classes annotated will get a default toJSON implementation compatible with
+   * the serialization algorithm implemented by the lib.
    */
+  // NOTE: This must be an arrow function so "this" can be bound to the class instance when
+  // called on the decorated class.
   serialize = <T extends Reviver>(target: T, _context: unknown = null) => {
     return this.serializeAs(target.name)(target, _context)
   }
 
-  serializeAs<T extends Reviver>(reviver: string) {
+  /**
+   * Decorator used to make a class serializable by @arborjs/json with a custom
+   * type serialization key.
+   *
+   * @param key key used to identify the class type so it can be deserialized back
+   * to the correct type.
+   */
+  serializeAs<T extends Reviver>(key: string) {
     return (target: T, _context: unknown = null) => {
-      target.prototype[ArborSerializeAs] = reviver || target.name
+      target.prototype[ArborSerializeAs] = key || target.name
 
       target.prototype.toJSON = function () {
         const $value = {}
@@ -61,6 +72,11 @@ export class Json {
     }
   }
 
+  /**
+   * Registers one or many {@link Reviver}s used to deserialize data.
+   *
+   * @param revivers a list of {@link Reviver} objects.
+   */
   register(...revivers: Reviver[]) {
     revivers.forEach((reviver) => {
       const key = reviver.prototype[ArborSerializeAs] || reviver.name
@@ -68,16 +84,38 @@ export class Json {
     })
   }
 
+  /**
+   * Registers a {@link Reviver} identified by the given key.
+   *
+   * This can be used to register revivers for different types sharing the same name.
+   *
+   * @param key key that identifies the {@link Reviver} object.
+   * @param reviver a {@link Reviver} to be used in the deserialization process.
+   */
   registerReviver(key: string, reviver: Reviver) {
     this.#revivers.set(key, reviver)
   }
 
+  /**
+   * Serializes an object into a JSON string while tracking which {@link Reviver}
+   * can be used to deserialize the value.
+   *
+   * @param value object to serialize into a string.
+   * @returns the string representation of the object.
+   */
   stringify(value: object): string {
     return JSON.stringify(value)
   }
 
-  parse<T>(obj: string): T {
-    return JSON.parse(obj, this.reviver.bind(this))
+  /**
+   * Parses a JSON string deserializing the data into its original type via
+   * the previously registered {@link Reviver} objects.
+   *
+   * @param value string to be parsed.
+   * @returns the deserialized data type represented by the given string.
+   */
+  parse<T>(value: string): T {
+    return JSON.parse(value, this.reviver.bind(this))
   }
 
   private reviver(_key: string, value: unknown) {
