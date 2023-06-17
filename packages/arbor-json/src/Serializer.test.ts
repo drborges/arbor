6ce8003819cfe70ad4d046773a19708548a43798
Diego Borges <drborges.cic@gmail.com>
2023-06-17 20:18:26 -0300

    Group test scenarios

--- packages/arbor-json/src/Serializer.test.ts
+++ packages/arbor-json/src/Serializer.test.ts
@@ -6,8 +6,6 @@ import {
   serializeAs,
 } from "./index"
 
-const json = new Json()
-
 class Todo {
   constructor(readonly uuid: string, public text: string) {}
 
@@ -23,24 +21,6 @@ class Todo {
         text: this.text,
       },
     }
-  }
-}
-
-@serialize
-class DecoratedTodo {
-  constructor(readonly uuid: string, public text: string) {}
-
-  static $revive(value: Serialized<DecoratedTodo>) {
-    return new DecoratedTodo(value.uuid, value.text)
-  }
-}
-
-@serializeAs("MyTodo")
-class DecoratedTodoCustomKey {
-  constructor(readonly uuid: string, public text: string) {}
-
-  static $revive(value: Serialized<DecoratedTodoCustomKey>) {
-    return new DecoratedTodoCustomKey(value.uuid, value.text)
   }
 }
 
@@ -62,118 +42,154 @@ class TodoList extends Map<string, Todo> {
 }
 
 describe("Serializer", () => {
-  it("serializes a simple object", () => {
-    const todo = { uuid: "a", text: "Clean the house" }
+  describe("simple object", () => {
+    it("serializes a simple object", () => {
+      const todo = { uuid: "a", text: "Clean the house" }
 
-    const serialized = json.stringify(todo)
+      const json = new Json()
+      const serialized = json.stringify(todo)
 
-    expect(serialized).toEqual('{"uuid":"a","text":"Clean the house"}')
+      expect(serialized).toEqual('{"uuid":"a","text":"Clean the house"}')
+    })
+
+    it("deserializes a simple object", () => {
+      const todo = { uuid: "a", text: "Clean the house" }
+
+      const json = new Json()
+      const serialized = json.stringify(todo)
+      const deserialized = json.parse(serialized)
+
+      expect(deserialized).toEqual(todo)
+    })
   })
 
-  it("deserializes a simple object", () => {
-    const todo = { uuid: "a", text: "Clean the house" }
+  describe("custom type", () => {
+    it("serializes a custom type", () => {
+      const todo = new Todo("a", "Clean the house")
 
-    const serialized = json.stringify(todo)
-    const deserialized = json.parse(serialized)
+      const json = new Json()
+      const serialized = json.stringify(todo)
 
-    expect(deserialized).toEqual(todo)
+      expect(serialized).toEqual(
+        '{"$reviver":"Todo","$value":{"id":"a","text":"Clean the house"}}'
+      )
+    })
+
+    it("deserializes a custom type", () => {
+      const json = new Json()
+      json.register(Todo)
+
+      const todo = new Todo("a", "Clean the house")
+
+      const serialized = json.stringify(todo)
+      const deserialized = json.parse(serialized)
+
+      expect(deserialized).toBeInstanceOf(Todo)
+      expect(deserialized).toEqual(todo)
+    })
+
+    it("serializes a nested object made out of custom types", () => {
+      const json = new Json()
+      const todoList = new TodoList(
+        new Todo("a", "Clean the house"),
+        new Todo("b", "Do the dishes"),
+        new Todo("c", "Walk the dogs")
+      )
+
+      const serialized = json.stringify(todoList)
+
+      expect(serialized).toEqual(
+        '{"$reviver":"TodoList","$value":[{"$reviver":"Todo","$value":{"id":"a","text":"Clean the house"}},{"$reviver":"Todo","$value":{"id":"b","text":"Do the dishes"}},{"$reviver":"Todo","$value":{"id":"c","text":"Walk the dogs"}}]}'
+      )
+    })
+
+    it("deserializes a nested object made out of custom types", () => {
+      const json = new Json()
+      json.register(Todo, TodoList)
+
+      const todoList = new TodoList(
+        new Todo("a", "Clean the house"),
+        new Todo("b", "Do the dishes"),
+        new Todo("c", "Walk the dogs")
+      )
+
+      const serialized = json.stringify(todoList)
+      const deserialized = json.parse(serialized)
+
+      expect(deserialized).toBeInstanceOf(TodoList)
+      expect(deserialized).toEqual(todoList)
+    })
   })
 
-  it("serializes a custom type", () => {
-    const todo = new Todo("a", "Clean the house")
+  describe("decorated custom type", () => {
+    @serialize
+    class DecoratedTodo {
+      constructor(readonly uuid: string, public text: string) {}
 
-    const serialized = json.stringify(todo)
+      static $revive(value: Serialized<DecoratedTodo>) {
+        return new DecoratedTodo(value.uuid, value.text)
+      }
+    }
 
-    expect(serialized).toEqual(
-      '{"$reviver":"Todo","$value":{"id":"a","text":"Clean the house"}}'
-    )
+    it("serializes a decorated custom type", () => {
+      const json = new Json()
+      json.register(DecoratedTodo)
+
+      const todo = new DecoratedTodo("a", "Clean the house")
+      const serialized = json.stringify(todo)
+
+      expect(serialized).toEqual(
+        '{"$value":{"uuid":"a","text":"Clean the house"},"$reviver":"DecoratedTodo"}'
+      )
+    })
+
+    it("deserializes a decorated custom type", () => {
+      const json = new Json()
+      json.register(DecoratedTodo)
+
+      const todo = new DecoratedTodo("a", "Clean the house")
+
+      const serialized = json.stringify(todo)
+      const deserialized = json.parse(serialized)
+
+      expect(deserialized).toBeInstanceOf(DecoratedTodo)
+      expect(deserialized).toEqual(todo)
+    })
   })
 
-  it("deserializes a custom type", () => {
-    json.register(Todo)
+  describe("decorated custom type with custom reviver key", () => {
+    @serializeAs("MyTodo")
+    class DecoratedTodoCustomKey {
+      constructor(readonly uuid: string, public text: string) {}
 
-    const todo = new Todo("a", "Clean the house")
+      static $revive(value: Serialized<DecoratedTodoCustomKey>) {
+        return new DecoratedTodoCustomKey(value.uuid, value.text)
+      }
+    }
 
-    const serialized = json.stringify(todo)
-    const deserialized = json.parse(serialized)
+    it("serializes a custom type decorated with @serializable and a custom reviver key", () => {
+      const json = new Json()
+      json.register(DecoratedTodoCustomKey)
 
-    expect(deserialized).toBeInstanceOf(Todo)
-    expect(deserialized).toEqual(todo)
-  })
+      const todo = new DecoratedTodoCustomKey("a", "Clean the house")
+      const serialized = json.stringify(todo)
 
-  it("serializes a custom type decorated with @serializable", () => {
-    json.register(DecoratedTodo)
+      expect(serialized).toEqual(
+        '{"$value":{"uuid":"a","text":"Clean the house"},"$reviver":"MyTodo"}'
+      )
+    })
 
-    const todo = new DecoratedTodo("a", "Clean the house")
-    const serialized = json.stringify(todo)
+    it("deserializes a custom type decorated with @serializable and a custom reviver key", () => {
+      const json = new Json()
+      json.register(DecoratedTodoCustomKey)
 
-    expect(serialized).toEqual(
-      '{"$value":{"uuid":"a","text":"Clean the house"},"$reviver":"DecoratedTodo"}'
-    )
-  })
+      const todo = new DecoratedTodoCustomKey("a", "Clean the house")
 
-  it("serializes a custom type decorated with @serializable and a custom reviver key", () => {
-    json.register(DecoratedTodoCustomKey)
+      const serialized = json.stringify(todo)
+      const deserialized = json.parse(serialized)
 
-    const todo = new DecoratedTodoCustomKey("a", "Clean the house")
-    const serialized = json.stringify(todo)
-
-    expect(serialized).toEqual(
-      '{"$value":{"uuid":"a","text":"Clean the house"},"$reviver":"MyTodo"}'
-    )
-  })
-
-  it("deserializes a custom type decorated with @serializable", () => {
-    json.register(DecoratedTodo)
-
-    const todo = new DecoratedTodo("a", "Clean the house")
-
-    const serialized = json.stringify(todo)
-    const deserialized = json.parse(serialized)
-
-    expect(deserialized).toBeInstanceOf(DecoratedTodo)
-    expect(deserialized).toEqual(todo)
-  })
-
-  it("deserializes a custom type decorated with @serializable and a custom reviver key", () => {
-    json.register(DecoratedTodoCustomKey)
-
-    const todo = new DecoratedTodoCustomKey("a", "Clean the house")
-
-    const serialized = json.stringify(todo)
-    const deserialized = json.parse(serialized)
-
-    expect(deserialized).toBeInstanceOf(DecoratedTodoCustomKey)
-    expect(deserialized).toEqual(todo)
-  })
-
-  it("serializes a nested object made out of custom types", () => {
-    const todoList = new TodoList(
-      new Todo("a", "Clean the house"),
-      new Todo("b", "Do the dishes"),
-      new Todo("c", "Walk the dogs")
-    )
-
-    const serialized = json.stringify(todoList)
-
-    expect(serialized).toEqual(
-      '{"$reviver":"TodoList","$value":[{"$reviver":"Todo","$value":{"id":"a","text":"Clean the house"}},{"$reviver":"Todo","$value":{"id":"b","text":"Do the dishes"}},{"$reviver":"Todo","$value":{"id":"c","text":"Walk the dogs"}}]}'
-    )
-  })
-
-  it("deserializes a nested object made out of custom types", () => {
-    json.register(Todo, TodoList)
-
-    const todoList = new TodoList(
-      new Todo("a", "Clean the house"),
-      new Todo("b", "Do the dishes"),
-      new Todo("c", "Walk the dogs")
-    )
-
-    const serialized = json.stringify(todoList)
-    const deserialized = json.parse(serialized)
-
-    expect(deserialized).toBeInstanceOf(TodoList)
-    expect(deserialized).toEqual(todoList)
+      expect(deserialized).toBeInstanceOf(DecoratedTodoCustomKey)
+      expect(deserialized).toEqual(todo)
+    })
   })
 })
