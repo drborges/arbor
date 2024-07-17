9b6df0dcb770d82bf05776b81486edf409861df5
Diego Borges <drborges.cic@gmail.com>
2024-07-17 15:13:18 -0300

    Remove dead code

--- packages/arbor-store/src/guards.ts
+++ packages/arbor-store/src/guards.ts
@@ -1,16 +1,10 @@
 import { Arbor } from "./arbor"
 import { ArborProxiable } from "./decorators"
-import type { ArborNode, Clonnable, Node } from "./types"
+import type { Clonnable, Node } from "./types"
 
 export function isNode<T extends object>(value: unknown): value is Node<T> {
   const isNodeValue = value as Node<T>
   return isNodeValue?.$tree instanceof Arbor
-}
-
-export function isArborNode<T extends object>(
-  value: unknown
-): value is ArborNode<T> {
-  return isNode(value)
 }
 
 export function isProxiable(value: unknown): value is object {
