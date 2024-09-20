fa8ec270715975644c5c0195b6fdd6f817bb8e3a
Diego Borges <drborges.cic@gmail.com>
2024-09-20 17:28:01 -0300

    Increase test coverage around short circuit logic and scoped stores

--- examples/cypress/component/ShortCircuit.cy.tsx
+++ examples/cypress/component/ShortCircuit.cy.tsx
@@ -5,5 +5,59 @@ import { App } from "../../short-circuit/App"
 describe("ShortCircuit App", () => {
   it("successfully renders components even when short circuit boolean logic is used", () => {
     cy.mount(<App />)
+
+    cy.contains("state.flags.flag1: false")
+    cy.contains("state.flags.flag2: false")
+    cy.contains("state.flags.flag3: false")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = false")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
+
+    cy.get("[data-testid='toggle-flag3']").click()
+
+    cy.contains("state.flags.flag1: false")
+    cy.contains("state.flags.flag2: false")
+    cy.contains("state.flags.flag3: true")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = true")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
+
+    cy.get("[data-testid='toggle-flag2']").click()
+
+    cy.contains("state.flags.flag1: false")
+    cy.contains("state.flags.flag2: true")
+    cy.contains("state.flags.flag3: true")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = true")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
+
+    cy.get("[data-testid='toggle-flag1']").click()
+
+    cy.contains("state.flags.flag1: true")
+    cy.contains("state.flags.flag2: true")
+    cy.contains("state.flags.flag3: true")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = true")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = true")
+
+    cy.get("[data-testid='toggle-flag3']").click()
+
+    cy.contains("state.flags.flag1: true")
+    cy.contains("state.flags.flag2: true")
+    cy.contains("state.flags.flag3: false")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = true")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
+
+    cy.get("[data-testid='toggle-flag2']").click()
+
+    cy.contains("state.flags.flag1: true")
+    cy.contains("state.flags.flag2: false")
+    cy.contains("state.flags.flag3: false")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = true")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
+
+    cy.get("[data-testid='toggle-flag1']").click()
+
+    cy.contains("state.flags.flag1: false")
+    cy.contains("state.flags.flag2: false")
+    cy.contains("state.flags.flag3: false")
+    cy.contains("app.flags.flag1 || app.flags.flag2 || app.flags.flag3 = false")
+    cy.contains("app.flags.flag1 && app.flags.flag2 && app.flags.flag3 = false")
   })
 })
--- examples/short-circuit/components/Actions.tsx
+++ examples/short-circuit/components/Actions.tsx
@@ -6,13 +6,22 @@ import { store } from "../store"
 export function Actions() {
   return (
     <Highlight label="<Actions />" key={Math.random()}>
-      <button onClick={store.state.flags.toggleFlag1}>
+      <button
+        data-testid="toggle-flag1"
+        onClick={store.state.flags.toggleFlag1}
+      >
         Toggle <code>store.state.flag1</code>
       </button>
-      <button onClick={store.state.flags.toggleFlag2}>
+      <button
+        data-testid="toggle-flag2"
+        onClick={store.state.flags.toggleFlag2}
+      >
         Toggle <code>store.state.flag2</code>
       </button>
-      <button onClick={store.state.flags.toggleFlag3}>
+      <button
+        data-testid="toggle-flag3"
+        onClick={store.state.flags.toggleFlag3}
+      >
         Toggle <code>store.state.flag3</code>
       </button>
     </Highlight>
