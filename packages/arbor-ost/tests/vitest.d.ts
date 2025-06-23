8fc2237a917ccfeaa2a612e8a683a4d5a0a59ebe
Diego Borges <drborges.cic@gmail.com>
2025-06-23 12:52:28 -0300

    Fix type declarations of vitest custom matchers

---
+++ packages/arbor-ost/tests/vitest.d.ts
@@ -0,0 +1,14 @@
+import "vitest"
+import { Node, Value } from "../src/types"
+import { OST } from "../src/ost"
+
+declare module "vitest" {
+  interface Assertion<T> {
+    toBeDetachedFrom: (ost: OST) => T
+    toBeNodeOf: (value: Value) => T
+    toBeRootNode: () => T
+    toHaveNodeFor: (value: Value) => T
+    toHaveParentNode: (parent?: Node) => T
+    toHaveNodeValuePair: (pair: [Node, Value]) => T
+  }
+}
